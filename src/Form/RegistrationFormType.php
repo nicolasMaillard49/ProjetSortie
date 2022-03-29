b2164d1724560d0b0b94e7812624e5cb3bee5389
suge49 <nicolas.maillard49320@gmail.com>
2022-03-29 14:08:32 +0200

    button agree item

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -27,14 +27,7 @@ class RegistrationFormType extends AbstractType
             ->add('site')
             //->add('actif')
            // ->add('organisateur')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -49,6 +42,14 @@ class RegistrationFormType extends AbstractType
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
                     ]),
                 ],
             ])
