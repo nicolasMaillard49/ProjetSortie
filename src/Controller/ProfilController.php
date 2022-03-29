371dbb62a2955a06212e74605222264835d64d9d
suge49 <nicolas.maillard49320@gmail.com>
2022-03-29 15:05:37 +0200

    login

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -7,9 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/profil", name="app_profil_")
- */
+
 class ProfilController extends AbstractController
 {
 
@@ -22,7 +20,7 @@ class ProfilController extends AbstractController
 
 
     /**
-     * @Route("/{id}", name="profil")
+     * @Route("profil/{id}", name="app_profil")
      */
     public function detail($id): Response
     {
