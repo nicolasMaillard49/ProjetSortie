8dba127848ffa9a6a339fafd8e1d46a7356d81de
HugoZak <zakrajsekhugo@gmail.com>
2022-03-29 09:51:12 +0200

    maj register + authenticator

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Participants;
+use App\Entity\Site;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -18,6 +19,13 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+            ->add('nom')
+            ->add('prenom')
+            ->add('pseudo')
+            ->add('tel')
+            ->add('site')
+            ->add('actif')
+            ->add('organisateur')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -36,7 +44,7 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
-                        'min' => 6,
+                        'min' => 3,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
