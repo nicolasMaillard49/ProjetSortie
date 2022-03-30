d2ce3d237cd74d8e25324acd34c64d1dd0d0f611
bgarniel2021 <brandon.garniel2021@campus-eni.fr>
2022-03-30 16:24:45 +0200

    maj page modifier profil

--- src/Form/ModifyUserType.php
+++ src/Form/ModifyUserType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\Participants;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +16,21 @@ class ModifyUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('nom')
-            ->add('prenom')
-            ->add('tel')
-            ->add('pseudo')
+            ->add('email',EmailType::class,[
+                'attr'=>["class"=>"border border-primary"]
+        ])
+            ->add('nom',TextType::class,[
+                'attr'=>["class"=>"border border-primary"]
+            ])
+            ->add('prenom', TextType::class,[
+                'attr'=>["class"=>"border border-primary"]
+            ])
+            ->add('tel', NumberType::class,[
+                'attr'=>["class"=>"border border-primary"]
+            ])
+            ->add('pseudo', TextType::class,[
+                'attr'=>["class"=>"border border-primary"]
+            ])
             ->add('Modifier', SubmitType::class)
         ;
     }
