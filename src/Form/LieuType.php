a93641e538b4852c2438c94035696a632b3e0b96
bgarniel2021 <brandon.garniel2021@campus-eni.fr>
2022-04-04 16:09:16 +0200

    maj gérer les lieux

---
+++ src/Form/LieuType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Site;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class LieuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('ville',EntityType::class,[
+                'class'=>Ville::class,'choice_label'=>'nom',
+                'expanded'=>false,'multiple'=>false,
+                'attr'=>["class"=>"border border-primary"]
+            ])
+            ->add('nom', TextType::class,[
+                'attr'=>["class"=>"border border-primary",
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                ]
+            ])
+            ->add('rue', TextType::class,[
+                'attr'=>["class"=>"border border-primary",
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                ]
+            ])->add('latitude', NumberType::class,[
+                'attr'=>["class"=>"border border-primary",
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                ]
+            ])->add('longitude', NumberType::class,[
+                'attr'=>["class"=>"border border-primary",
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                    new NotNull(),
+                ]
+            ])
+
+            ->add('ajouter', SubmitType::class,[
+                'attr'=>["class"=>"btn btn-success"]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lieu::class,
+        ]);
+    }
+}
